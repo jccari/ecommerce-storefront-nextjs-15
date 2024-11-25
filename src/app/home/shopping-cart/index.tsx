1597e4e6c53bf640a714b59081aeec6d7ca6be26
Jhoel Ccari <jhoelccari.23@gmail.com>
2024-11-25 01:56:19 -0500

    enhance empty cart styles

--- src/app/home/shopping-cart/index.tsx
+++ src/app/home/shopping-cart/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 import ProductLineItem from "@/components/product-line-item";
 import useCartStore from "@/stores/use-cart-store";
 
@@ -8,7 +10,8 @@ function ShoppingCartContent() {
 
   if (cart.length === 0) {
     return (
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col flex-1 self-center gap-2 items-center">
+        <Image src="empty-cart.svg" alt="Empty Cart" width={50} height={50} />
         <h3 className="font-bold">Your cart is empty</h3>
       </div>
     );
@@ -41,7 +44,7 @@ function ShoppingCart() {
 
   return (
     <div className="basis-1/4">
-      <div className="flex flex-col h-2/3 w-full rounded-lg border-4 border-teal-600 bg-slate-100">
+      <div className="flex flex-col h-2/3 p-2 w-full rounded-lg border-4 bg-slate-100">
         <div className="flex justify-between basis-1/12 p-2">
           <h2 className="font-bold pb-5 text-lg">Shopping Cart</h2>
           <button
@@ -51,10 +54,10 @@ function ShoppingCart() {
             Clear Cart
           </button>
         </div>
-        <div className="basis-10/12 grow-0">
+        <div className="flex basis-10/12 grow-0 border-t-4">
           <ShoppingCartContent />
         </div>
-        <div className="basis-1/12 p-2">
+        <div className="basis-1/12">
           <div className="border-t-4 text-sm">
             <p className="font-semibold mb-2">Resumen de compra</p>
             <PriceLine concept="Productos" value={priceLines.lineItemsTotal} />
