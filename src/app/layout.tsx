7b6c4560cdaa29f0ed2247ac09da6b9327fd43fb
Jhoel Ccari <jhoelccari.23@gmail.com>
2025-01-29 15:50:35 -0500

    enable react scan by env variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
-          src="https://unpkg.com/react-scan/dist/auto.global.js"
-          async
-        ></script>
+        {process.env.ENABLE_REACT_SCAN === "true" && (
+          <script
+            src="https://unpkg.com/react-scan/dist/auto.global.js"
+            async
+          />
+        )}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={"antialiased h-screen w-screen bg-gray-200"}>
